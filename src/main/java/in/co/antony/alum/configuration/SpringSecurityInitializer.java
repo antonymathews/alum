2839ba2df6ee06b7fa19f3cb9602c5910430265a
Antony Mathews <antony.mathews@gmail.com>
2016-03-16 00:24:40 +0530

    contributor name change;

--- src/main/java/in/co/antony/alum/configuration/SpringSecurityInitializer.java
+++ src/main/java/in/co/antony/alum/configuration/SpringSecurityInitializer.java
@@ -5,7 +5,7 @@ With inputs from
 Biajay Sahoo			batch-2001
 Sundeep Mohanty (Tutu)	batch-2001
 Sambit Satpathy			batch-2000
-Soumya Mohanty (Bapi) 	batch-2001
+Soumya Ranjan Parida (Bapi) 	batch-2001
 Kamalesh Nayak			batch-2001
  -->
 
